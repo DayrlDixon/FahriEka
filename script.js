b0979ce17e92887d450f89e6013c83b9a4a7f691
Fahri <fahri.051000@gmail.com>
2025-09-21 20:38:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // --- GANTI URL INI ---
     // Tempel (paste) URL Web App yang Anda dapat dari Langkah 2 di sini
-    const SHEET_URL = "https://script.google.com/macros/library/d/1zB9C6tr8p7lDnl8PR0uywW13ivv-0SrdXd5eHhvTawO36kT_nCwdmDXm/5";
+    const SHEET_URL = "https://script.google.com/macros/s/AKfycbyMcPi2E7bpRe2iQYPhfrUSSwCMpVlL5WdtKq5sjsQLDYxD_MURo4YBY3c8R2T2P7u6/exec";
 
     // --- Pemilihan Elemen DOM ---
     const cover = document.getElementById('cover');
@@ -165,6 +165,7 @@ document.addEventListener('DOMContentLoaded', () => {
     setupOtherFeatures();
     setupScrollAnimations();
 });
+
 
 
 
